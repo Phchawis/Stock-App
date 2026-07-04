6c796b13256e3a59c828fd2ed8b29c2dd2f78966
Phchawis <gpharkchawisp@gmail.com>
2026-07-05 01:38:27 +0700

    Update IssueModal.jsx

--- src/screens/IssueModal.jsx
+++ src/screens/IssueModal.jsx
@@ -173,8 +173,8 @@ export function IssueModal({ v }) {
             <button
               onClick={closeModal}
               style={css(modalHeaderCloseStyle)}
-              onMouseEnter={(e) => { e.currentTarget.style.background = 'rgba(0,0,0,.1)'; }}
-              onMouseLeave={(e) => { e.currentTarget.style.background = 'rgba(0,0,0,.05)'; }}
+              onMouseEnter={(e) => { e.currentTarget.style.background = 'rgba(255,255,255,.55)'; }}
+              onMouseLeave={(e) => { e.currentTarget.style.background = 'rgba(255,255,255,.32)'; }}
             >
               {ic.close}
             </button>
