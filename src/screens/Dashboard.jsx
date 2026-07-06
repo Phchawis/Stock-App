a99411129354c5eaa0058909c9a503247a9fadd5
Phchawis <gpharkchawisp@gmail.com>
2026-07-06 14:00:55 +0700

    Update Dashboard.jsx

--- src/screens/Dashboard.jsx
+++ src/screens/Dashboard.jsx
@@ -144,7 +144,7 @@ export function Dashboard({ v }) {
     @media print {
       @page {
         size: A4 portrait;
-        margin: 16mm 14mm;
+        margin: 2cm;
       }
       *, *::before, *::after {
         background-color: transparent !important;
